e3d0d1c0199d5e87ac26691081e8f46f71dd6446
Zhenkai Zhu <zhenkai@cs.ucla.edu>
2012-10-16 13:19:19 -0700

    test

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+#include <boost/python.hpp>
+#include <string>
+#include <iostream>
+#include "world.cpp"
+
+using namespace boost::python;
+
+BOOST_PYTHON_MODULE(hello)
+{
+  class_<World>("World", init<std::string, object>())
+    .def("say", &World::say)
+    ;
+}
